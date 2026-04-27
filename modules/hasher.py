7f7038faefaf0a7dbb3c9800094d235eb4f12414
TegarPratama <kandolsaja@gmail.com>
2026-04-28 00:28:46 +0700

    Update hasher.py

--- modules/hasher.py
+++ modules/hasher.py
@@ -8,15 +8,4 @@ class HashTools:
         print(f"\n\033[93mMD5\033[0m     : {hashlib.md5(text.encode()).hexdigest()}")
         print(f"\033[93mSHA1\033[0m    : {hashlib.sha1(text.encode()).hexdigest()}")
         print(f"\033[93mSHA256\033[0m  : {hashlib.sha256(text.encode()).hexdigest()}")
-    
-    @staticmethod
-    def hash_file(filepath):
-        try:
-            with open(filepath, 'rb') as f:
-                data = f.read()
-                print(f"\n\033[92m📁 File: {filepath}\033[0m")
-                print(f"\n\033[93mMD5\033[0m    : {hashlib.md5(data).hexdigest()}")
-                print(f"\033[93mSHA256\033[0m : {hashlib.sha256(data).hexdigest()}")
-        except FileNotFoundError:
-            print("\n\033[91m❌ File tidak ditemukan!\033[0m")
 EOF
